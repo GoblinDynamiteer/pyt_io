538884c5ca0dad643efa9de6637e6de24452c95d
Johan Kämpe <kampe.johan@gmail.com>
2018-03-27 22:53:58 +0200

    user_input: fix pr class/obj referencing

--- user_input.py
+++ user_input.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-import sys, printout
+import sys, printout, os
 from printout import print_class as pr
+
+pr1 = pr(os.path.basename(__file__))
 
 def yes_no(question, default="yes", script_name=None):
     valid = {"yes": True, "y": True, "no": False, "n": False}
@@ -11,19 +13,18 @@ def yes_no(question, default="yes", script_name=None):
     elif default == "no":
         prompt = " [y/N] "
     else:
-        pr.error("wrong default")
+        pr1.error("wrong default")
         quit()
-
     while True:
         if script_name:
-            pr = pr(script_name)
+            pr2 = pr(script_name)
         else:
-            pr = pr(os.path.basename(__file__))
-        pr.info("{} {}".format(question, prompt), end_line=False)
+            pr2 = pr(os.path.basename(__file__))
+        pr2.info("{} {}".format(question, prompt), end_line=False)
         choice = input().lower()
         if default is not None and choice is '':
             return valid[default]
         elif choice in valid:
             return valid[choice]
         else:
-            pr.error("Enter Yes/No!")
+            pr2.error("Enter Yes/No!")
